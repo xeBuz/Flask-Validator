b1166e7ab90b98d414085b532f26fed5653a2f0c
Jesus Roldan <xebuz18@gmail.com>
2015-11-19 15:12:01 -0300

    Added Imports compatible with Python3

--- flask_validator/__init__.py
+++ flask_validator/__init__.py
@@ -1,5 +1,5 @@
 from .validator import FlaskValidator, Validator
 
-from constraints import ValidateInteger, ValidateNumeric, ValidateString, ValidateBoolean, ValidateLength, \
+from .constraints import ValidateInteger, ValidateNumeric, ValidateString, ValidateBoolean, ValidateLength, \
     ValidateLessThan, ValidateLessThanOrEqual, ValidateGreaterThan, ValidateGreaterThanOrEqual, ValidateEmail, \
     ValidateIP, ValidateURL, ValidateRegex
--- flask_validator/constraints/__init__.py
+++ flask_validator/constraints/__init__.py
@@ -1,5 +1,5 @@
-from types import ValidateInteger, ValidateNumeric, ValidateString, ValidateBoolean
-from numbers import ValidateLength
-from comparison import ValidateLessThan, ValidateLessThanOrEqual, ValidateGreaterThan, ValidateGreaterThanOrEqual
-from internet import ValidateEmail, ValidateIP, ValidateURL
-from others import ValidateRegex
+from .types import ValidateInteger, ValidateNumeric, ValidateString, ValidateBoolean
+from .numbers import ValidateLength
+from .comparison import ValidateLessThan, ValidateLessThanOrEqual, ValidateGreaterThan, ValidateGreaterThanOrEqual
+from .internet import ValidateEmail, ValidateIP, ValidateURL
+from .others import ValidateRegex
