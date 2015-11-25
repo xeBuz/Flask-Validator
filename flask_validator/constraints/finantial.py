3ea8291afa1fee19700d2148bfd5c534f92ff870
Jesus Roldan <xebuz18@gmail.com>
2015-11-25 15:52:30 -0300

    Ugly fix for Python3.X

--- flask_validator/constraints/finantial.py
+++ flask_validator/constraints/finantial.py
@@ -1,3 +1,6 @@
+import sys
+import string
+import re
 from flask_validator import Validator
 
 
@@ -14,25 +17,149 @@ class ValidateCreditCard(Validator):
         field: SQLAlchemy column to validate
         allow_null: (bool) Allow null values
         throw_exception: (bool) Throw a ValidateError if the validation fails
-
     """
 
     def check_value(self, value):
-        n = str(value).translate(None, ' -')
+        if sys.version_info >= (3, 0):
+            n = str(value).replace(' ', '').replace('-', '')
+        else:
+            n = str(value).translate(None, ' -')
+
         r = [int(ch) for ch in str(n)][::-1]
         return (sum(r[0::2]) + sum(sum(divmod(d*2,10)) for d in r[1::2])) % 10 == 0
 
 
-class CurrencyConstraint(Validator):
-    # TODO
-    def check_value(self, value):
-        pass
+class ValidateCurrency(Validator):
 
+    """ Validate Currency
 
-class LuhnConstraint(Validator):
-    # TODO
+    Check if the new value is valida credit card number.
+    List provided by: https://github.com/iktw/python-currency-list/blob/master/python_currency_dict.py
+
+    Args:
+        field: SQLAlchemy column to validate
+        allow_null: (bool) Allow null values
+        throw_exception: (bool) Throw a ValidateError if the validation fails
+    """
+    currency = {
+        'ALL': 'Albania Lek',
+        'AFN': 'Afghanistan Afghani',
+        'ARS': 'Argentina Peso',
+        'AWG': 'Aruba Guilder',
+        'AUD': 'Australia Dollar',
+        'AZN': 'Azerbaijan New Manat',
+        'BSD': 'Bahamas Dollar',
+        'BBD': 'Barbados Dollar',
+        'BDT': 'Bangladeshi taka',
+        'BYR': 'Belarus Ruble',
+        'BZD': 'Belize Dollar',
+        'BMD': 'Bermuda Dollar',
+        'BOB': 'Bolivia Boliviano',
+        'BAM': 'Bosnia and Herzegovina Convertible Marka',
+        'BWP': 'Botswana Pula',
+        'BGN': 'Bulgaria Lev',
+        'BRL': 'Brazil Real',
+        'BND': 'Brunei Darussalam Dollar',
+        'KHR': 'Cambodia Riel',
+        'CAD': 'Canada Dollar',
+        'KYD': 'Cayman Islands Dollar',
+        'CLP': 'Chile Peso',
+        'CNY': 'China Yuan Renminbi',
+        'COP': 'Colombia Peso',
+        'CRC': 'Costa Rica Colon',
+        'HRK': 'Croatia Kuna',
+        'CUP': 'Cuba Peso',
+        'CZK': 'Czech Republic Koruna',
+        'DKK': 'Denmark Krone',
+        'DOP': 'Dominican Republic Peso',
+        'XCD': 'East Caribbean Dollar',
+        'EGP': 'Egypt Pound',
+        'SVC': 'El Salvador Colon',
+        'EEK': 'Estonia Kroon',
+        'EUR': 'Euro Member Countries',
+        'FKP': 'Falkland Islands (Malvinas) Pound',
+        'FJD': 'Fiji Dollar',
+        'GHC': 'Ghana Cedis',
+        'GIP': 'Gibraltar Pound',
+        'GTQ': 'Guatemala Quetzal',
+        'GGP': 'Guernsey Pound',
+        'GYD': 'Guyana Dollar',
+        'HNL': 'Honduras Lempira',
+        'HKD': 'Hong Kong Dollar',
+        'HUF': 'Hungary Forint',
+        'ISK': 'Iceland Krona',
+        'INR': 'India Rupee',
+        'IDR': 'Indonesia Rupiah',
+        'IRR': 'Iran Rial',
+        'IMP': 'Isle of Man Pound',
+        'ILS': 'Israel Shekel',
+        'JMD': 'Jamaica Dollar',
+        'JPY': 'Japan Yen',
+        'JEP': 'Jersey Pound',
+        'KZT': 'Kazakhstan Tenge',
+        'KPW': 'Korea (North) Won',
+        'KRW': 'Korea (South) Won',
+        'KGS': 'Kyrgyzstan Som',
+        'LAK': 'Laos Kip',
+        'LVL': 'Latvia Lat',
+        'LBP': 'Lebanon Pound',
+        'LRD': 'Liberia Dollar',
+        'LTL': 'Lithuania Litas',
+        'MKD': 'Macedonia Denar',
+        'MYR': 'Malaysia Ringgit',
+        'MUR': 'Mauritius Rupee',
+        'MXN': 'Mexico Peso',
+        'MNT': 'Mongolia Tughrik',
+        'MZN': 'Mozambique Metical',
+        'NAD': 'Namibia Dollar',
+        'NPR': 'Nepal Rupee',
+        'ANG': 'Netherlands Antilles Guilder',
+        'NZD': 'New Zealand Dollar',
+        'NIO': 'Nicaragua Cordoba',
+        'NGN': 'Nigeria Naira',
+        'NOK': 'Norway Krone',
+        'OMR': 'Oman Rial',
+        'PKR': 'Pakistan Rupee',
+        'PAB': 'Panama Balboa',
+        'PYG': 'Paraguay Guarani',
+        'PEN': 'Peru Nuevo Sol',
+        'PHP': 'Philippines Peso',
+        'PLN': 'Poland Zloty',
+        'QAR': 'Qatar Riyal',
+        'RON': 'Romania New Leu',
+        'RUB': 'Russia Ruble',
+        'SHP': 'Saint Helena Pound',
+        'SAR': 'Saudi Arabia Riyal',
+        'RSD': 'Serbia Dinar',
+        'SCR': 'Seychelles Rupee',
+        'SGD': 'Singapore Dollar',
+        'SBD': 'Solomon Islands Dollar',
+        'SOS': 'Somalia Shilling',
+        'ZAR': 'South Africa Rand',
+        'LKR': 'Sri Lanka Rupee',
+        'SEK': 'Sweden Krona',
+        'CHF': 'Switzerland Franc',
+        'SRD': 'Suriname Dollar',
+        'SYP': 'Syria Pound',
+        'TWD': 'Taiwan New Dollar',
+        'THB': 'Thailand Baht',
+        'TTD': 'Trinidad and Tobago Dollar',
+        'TRY': 'Turkey Lira',
+        'TRL': 'Turkey Lira',
+        'TVD': 'Tuvalu Dollar',
+        'UAH': 'Ukraine Hryvna',
+        'GBP': 'United Kingdom Pound',
+        'USD': 'United States Dollar',
+        'UYU': 'Uruguay Peso',
+        'UZS': 'Uzbekistan Som',
+        'VEF': 'Venezuela Bolivar',
+        'VND': 'Viet Nam Dong',
+        'YER': 'Yemen Rial',
+        'ZWD': 'Zimbabwe Dollar'
+    }
+
     def check_value(self, value):
-        pass
+        return bool(self.currency.get(value))
 
 
 class IbanConstraint(Validator):
