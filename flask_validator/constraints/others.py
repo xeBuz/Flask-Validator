ef498cbd98d36522351925c83b91351740b7bf3a
Jesus Roldan <xebuz18@gmail.com>
2016-03-04 10:19:26 -0300

    ValidateRange added

--- flask_validator/constraints/others.py
+++ flask_validator/constraints/others.py
@@ -78,3 +78,29 @@ class ValidateISBN(Validator):
     """
     def check_value(self, value):
         return is_isbn10(value) or is_isbn13(value)
+
+
+class ValidateRange(Validator):
+    """ Validate Range
+
+    Validate if the value is in a specific value range
+
+    Args:
+        field: SQLAlchemy column to validate
+        allow_null: (bool) Allow null values
+        throw_exception: (bool) Throw a ValidateError if the validation fails
+
+    """
+    range = []
+
+    def __init__(self, field, range=[], allow_null=True, throw_exception=False, message=None):
+        self.range = range
+
+        Validator.__init__(self, field, allow_null, throw_exception, message)
+
+    def check_value(self, value):
+        try:
+            return value in self.range
+        except TypeError:
+            return False
+
