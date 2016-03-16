776540f6e1043b4b03f7b0a61e75a4f91e6c7433
Jesus Roldan <xebuz18@gmail.com>
2016-03-16 14:04:27 -0300

    New release 1.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
 
 setup(
     name='Flask-Validator',
-    version='1.2.0',
+    version='1.2.1',
     license='Mozilla Public License',
     author='Jesus Roldan',
     author_email='jesus.roldan@gmail.com',
