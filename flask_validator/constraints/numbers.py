1ba995c2f29f3990789f1d7db2f2d3301d4468d5
tristanto <tristanto.kurniawan@gmail.com>
2017-05-16 22:26:22 +0700

    Fix typo :hammer:

--- flask_validator/constraints/numbers.py
+++ flask_validator/constraints/numbers.py
@@ -9,17 +9,17 @@ class ValidateLength(Validator):
     Args:
         field: SQLAlchemy column to validate
         max_length: (int) Maximum value length
-        min_lenght: (int) Minimum value length
+        min_length: (int) Minimum value length
         throw_exception: (bool) Throw a ValidateError if the validation fails
 
     """
 
     max_length = None
-    min_lenght = 0
+    min_length = 0
 
     def __init__(self, field, max_length=None, min_length=0, throw_exception=False, message=None):
         self.max_length = max_length
-        self.min_lenght = min_length
+        self.min_length = min_length
 
         Validator.__init__(self, field, False, throw_exception, message)
 
