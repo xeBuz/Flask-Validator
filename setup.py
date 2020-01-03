8263f5d4608b61e783b90cd94168e7037705421a
Jesús Roldán <jesus.roldan@gmail.com>
2020-01-03 15:33:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
